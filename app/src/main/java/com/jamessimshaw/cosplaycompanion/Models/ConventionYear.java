f524f3e282a744543c52185d91d3d905bbd7385d
James Simshaw <james.simshaw@gmail.com>
2015-09-25 12:11:52 -0700

    Created Convention Year model

---
+++ app/src/main/java/com/jamessimshaw/cosplaycompanion/Models/ConventionYear.java
@@ -0,0 +1,45 @@
+package com.jamessimshaw.cosplaycompanion.Models;
+
+import java.util.Date;
+
+/**
+ * Created by james on 9/25/15.
+ */
+public class ConventionYear {
+    private Date mDate;
+    private int mDays;
+    private int mId;
+    private int mConventionId;
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public int getDays() {
+        return mDays;
+    }
+
+    public void setDays(int days) {
+        mDays = days;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
+    }
+
+    public int getConventionId() {
+        return mConventionId;
+    }
+
+    public void setConventionId(int conventionId) {
+        mConventionId = conventionId;
+    }
+}
