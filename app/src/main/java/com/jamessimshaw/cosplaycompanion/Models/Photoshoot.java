dfc4fd8accb8fac9e3c4b83faf552441508ec3a3
James Simshaw <james.simshaw@gmail.com>
2015-09-25 12:25:50 -0700

    Created Photoshoot model

---
+++ app/src/main/java/com/jamessimshaw/cosplaycompanion/Models/Photoshoot.java
@@ -0,0 +1,63 @@
+package com.jamessimshaw.cosplaycompanion.Models;
+
+import java.util.Date;
+
+/**
+ * Created by james on 9/25/15.
+ */
+public class Photoshoot {
+    private String mSeries;
+    private Date mStart;
+    private String mLocation;
+    private String mDescription;
+    private int mId;
+    private int mConventionYearId;
+
+    public String getSeries() {
+        return mSeries;
+    }
+
+    public void setSeries(String series) {
+        mSeries = series;
+    }
+
+    public Date getStart() {
+        return mStart;
+    }
+
+    public void setStart(Date start) {
+        mStart = start;
+    }
+
+    public String getLocation() {
+        return mLocation;
+    }
+
+    public void setLocation(String location) {
+        mLocation = location;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
+    }
+
+    public int getConventionYearId() {
+        return mConventionYearId;
+    }
+
+    public void setConventionYearId(int conventionYearId) {
+        mConventionYearId = conventionYearId;
+    }
+}
