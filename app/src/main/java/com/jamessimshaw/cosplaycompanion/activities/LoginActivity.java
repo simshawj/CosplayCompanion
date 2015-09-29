96d1f40e7d632bedceb8cc13d88ea7cbca63eb68
James Simshaw <james.simshaw@gmail.com>
2015-09-28 20:10:49 -0700

    Create MainActivity, have a toolbar appear, and have LoginActivity go to MainActivity on Login

--- app/src/main/java/com/jamessimshaw/cosplaycompanion/activities/LoginActivity.java
+++ app/src/main/java/com/jamessimshaw/cosplaycompanion/activities/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.jamessimshaw.cosplaycompanion.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.jamessimshaw.cosplaycompanion.R;
 
@@ -12,5 +15,14 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button) findViewById(R.id.login_btn_sign_in);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
