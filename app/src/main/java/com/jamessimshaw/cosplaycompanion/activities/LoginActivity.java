e8f3eafb8f24e1173228780a434e30c815e210f9
James Simshaw <james.simshaw@gmail.com>
2015-10-15 22:09:09 -0700

    Temporarily bypass the login screen until user authentication is setup

--- app/src/main/java/com/jamessimshaw/cosplaycompanion/activities/LoginActivity.java
+++ app/src/main/java/com/jamessimshaw/cosplaycompanion/activities/LoginActivity.java
@@ -16,13 +16,7 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button loginButton = (Button) findViewById(R.id.login_btn_sign_in);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
