b9472e116641aaf6e2ad3b5de4d54e53d725484c
James Simshaw <james.simshaw@gmail.com>
2016-09-05 13:24:17 -0700

    Removed some test files to setup for new testing structure.

--- app/src/test/java/com/jamessimshaw/cosplaycompanion/presenters/ListConventionsPresenterImplTest.java
+++ app/src/test/java/com/jamessimshaw/cosplaycompanion/presenters/ListConventionsPresenterImplTest.java
@@ -1,8 +1,12 @@
 package com.jamessimshaw.cosplaycompanion.presenters;
 
+import com.jamessimshaw.cosplaycompanion.BuildConfig;
 import com.jamessimshaw.cosplaycompanion.views.ListConventionsView;
 
 import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
@@ -10,6 +14,8 @@ import static org.mockito.Mockito.mock;
 /**
  * Created by james on 3/22/16.
  */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
 public class ListConventionsPresenterImplTest {
 
     private ListConventionsView mView;
