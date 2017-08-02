40c86d529e9f0fc3a1d03d6bca1c6be52601aa0e
James Simshaw <james.simshaw@gmail.com>
2017-08-01 21:02:36 -0700

    Start of integrating google sign in

--- app/src/main/java/com/jamessimshaw/cosplaycompanion/activities/LoginActivity.java
+++ app/src/main/java/com/jamessimshaw/cosplaycompanion/activities/LoginActivity.java
@@ -3,9 +3,16 @@ package com.jamessimshaw.cosplaycompanion.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.jamessimshaw.cosplaycompanion.CosplayCompanionApplication;
 import com.jamessimshaw.cosplaycompanion.R;
 import com.jamessimshaw.cosplaycompanion.presenters.LoginPresenter;
@@ -19,17 +26,30 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 
-public class LoginActivity extends Activity implements LoginView {
+public class LoginActivity extends AppCompatActivity implements LoginView, GoogleApiClient.OnConnectionFailedListener {
 
     @BindView(R.id.login_edit_username) EditText mLoginNameEditText;
     @BindView(R.id.login_edit_password) EditText mPasswordEditText;
 
+    public static int SIGN_IN_REQUEST_CODE = 2134;
+
     @Inject LoginPresenter mPresenter;
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
         ButterKnife.bind(this);
     }
@@ -59,6 +79,12 @@ public class LoginActivity extends Activity implements LoginView {
         Toast.makeText(this, "This feature is not yet implemented", Toast.LENGTH_LONG).show();
     } */
 
+    @OnClick(R.id.login_btn_google)
+    public void signin() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, SIGN_IN_REQUEST_CODE);
+    }
+
     @OnClick(R.id.login_btn_register)
     public void register() {
         Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
@@ -69,6 +95,17 @@ public class LoginActivity extends Activity implements LoginView {
     @OnClick(R.id.login_btn_sign_in)
     public void login() {
         mPresenter.login();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            //handleSignInResult(result);
+        }
+
     }
 
     // LoginView Methods
@@ -92,5 +129,12 @@ public class LoginActivity extends Activity implements LoginView {
     public void done() {
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
+    }
+
+    // GoogleApiClient.OnConnectionFailedListener method
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
