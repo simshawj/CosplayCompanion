f65c55b8b37896ca01350f147884ba86832f79c0
James Simshaw <james.simshaw@gmail.com>
2018-11-09 14:59:40 -0800

    Updated the LoginActivity to Kotlin and fixed the signin process

--- app/src/main/java/com/jamessimshaw/cosplaycompanion/activities/LoginActivity.kt
+++ app/src/main/java/com/jamessimshaw/cosplaycompanion/activities/LoginActivity.kt
@@ -3,69 +3,63 @@ package com.jamessimshaw.cosplaycompanion.activities
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import androidx.annotation.NonNull
-import androidx.appcompat.app.AppCompatActivity
 import android.text.Html
 import android.text.method.LinkMovementMethod
+import android.util.Log
 import android.widget.TextView
-import android.widget.Toast
-
-import com.google.android.gms.auth.api.Auth
+import androidx.appcompat.app.AppCompatActivity
+import butterknife.BindView
+import butterknife.ButterKnife
+import butterknife.OnClick
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.ConnectionResult
-import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.jamessimshaw.cosplaycompanion.R
 
-import butterknife.BindView
-import butterknife.ButterKnife
-import butterknife.OnClick
 
+class LoginActivity : AppCompatActivity() {
 
-class LoginActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener {
-
-    private var mGoogleApiClient: GoogleApiClient? = null
-    private var mFirebaseAuth: FirebaseAuth? = null
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var firebaseAuth: FirebaseAuth
 
     @BindView(R.id.termsTextView)
-    internal var mTermsTextView: TextView? = null
+    internal var termsTextView: TextView? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        mFirebaseAuth = FirebaseAuth.getInstance()
+        firebaseAuth = FirebaseAuth.getInstance()
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build()
 
-        mGoogleApiClient = GoogleApiClient.Builder(this)
-                .enableAutoManage(this, this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
         ButterKnife.bind(this)
 
         val htmlString = getString(R.string.terms_privacy_agreement)
 
         if (Build.VERSION.SDK_INT > 23)
-            mTermsTextView!!.text = Html.fromHtml(htmlString, Html.FROM_HTML_MODE_LEGACY)
+            termsTextView!!.text = Html.fromHtml(htmlString, Html.FROM_HTML_MODE_LEGACY)
         else
-            mTermsTextView!!.text = Html.fromHtml(htmlString)
+            termsTextView!!.text = Html.fromHtml(htmlString)
 
-        mTermsTextView!!.isClickable = true
-        mTermsTextView!!.movementMethod = LinkMovementMethod.getInstance()
+        termsTextView!!.isClickable = true
+        termsTextView!!.movementMethod = LinkMovementMethod.getInstance()
     }
 
     override fun onStart() {
         super.onStart()
 
-        val user = mFirebaseAuth!!.currentUser
+        val user = firebaseAuth.currentUser
         verifyUser(user)
     }
 
@@ -81,7 +75,7 @@ class LoginActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedLis
 
     @OnClick(R.id.login_btn_google)
     fun signin() {
-        val signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
+        val signInIntent = googleSignInClient.signInIntent
         startActivityForResult(signInIntent, SIGN_IN_REQUEST_CODE)
     }
 
@@ -89,28 +83,22 @@ class LoginActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedLis
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == SIGN_IN_REQUEST_CODE) {
-            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
-            if (result.isSuccess) {
-                val googleAccount = result.signInAccount
-                val authCredential = GoogleAuthProvider.getCredential(googleAccount!!.idToken, null)
-                mFirebaseAuth!!.signInWithCredential(authCredential).addOnCompleteListener(this) { task ->
+            val googleSignInTask = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = googleSignInTask.getResult(ApiException::class.java)
+                val authCredential = GoogleAuthProvider.getCredential(account!!.idToken, null)
+                firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
-                        val user = mFirebaseAuth!!.currentUser
+                        val user = firebaseAuth.currentUser
                         verifyUser(user)
                     }
                 }
-
-            } else {
-                Toast.makeText(this, "Unable to sign in", Toast.LENGTH_SHORT).show()
+            } catch (e: ApiException) {
+                // TODO: Google Sign In failed, update UI appropriately
+                Log.d("Signin", "Google sign in failed", e)
             }
-            //handleSignInResult(result);
         }
-
-    }
-
-    // GoogleApiClient.OnConnectionFailedListener method
-
-    override fun onConnectionFailed(@NonNull connectionResult: ConnectionResult) {
 
     }
 
