483ab57d5a4a136a33f77e99281eb1c61ce3529f
James Simshaw <james.simshaw@gmail.com>
2019-05-26 12:35:19 -0700

    Convert MainActivity to Kotlin and setup Navigation Component with placeholders

---
+++ app/src/main/java/com/jamessimshaw/cosplaycompanion/activities/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.jamessimshaw.cosplaycompanion.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jamessimshaw.cosplaycompanion.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
